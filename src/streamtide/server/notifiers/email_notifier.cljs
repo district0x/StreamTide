8adfb11e96a1f228eb455f66b8e856da54c6ba87
ginesdt <ginesdt@gmail.com>
2023-09-22 11:27:04 +0200

    implement email notifications

--- src/streamtide/server/notifiers/email_notifier.cljs
+++ src/streamtide/server/notifiers/email_notifier.cljs
@@ -1,6 +1,7 @@
 (ns streamtide.server.notifiers.email-notifier
   (:require
     [district.server.config :refer [config]]
+    [district.sendgrid :as sendgrid]
     [district.shared.async-helpers :refer [<? safe-go]]
     [streamtide.server.db :as stdb]
     [streamtide.server.notifiers.notifiers :refer [notify store-id get-ids]]
@@ -22,13 +23,30 @@
   (stdb/get-notification-types {:user/addresses addresses
                                 :notification/type notifier-type}))
 
+(defn send-email [email message]
+  (safe-go
+    (let [{:keys [:from :template-id :api-key :print-mode?]} (-> @config :notifiers :email)]
+      (sendgrid/send-email
+        (cond->
+          {:from from
+           :to email
+           :subject (:title message)
+           :content (str (:body message))
+           :api-key api-key
+           :print-mode? print-mode?
+           :on-error #(log/error "Error sending email" {:error % :to email})
+           :on-success #(log/debug "Email sent" {:message message :to email})}
+          template-id (merge {:template-id template-id
+                              :dynamic-template-data {:title (:title message)
+                                                      :body (:body message)}}))))))
+
 (defn email-notify [user-entries {:keys [:title :body] :as message}]
   (safe-go
     (loop [user-entries user-entries]
       (when user-entries
         (let [user-entry (first user-entries)]
-          ; TODO
           (log/debug "Sending email" (merge user-entry message))
+          (send-email (:notification/user-id user-entry) message)
           (recur (next user-entries)))))))
 
 (defmethod notify notifier-type-kw [_ users notification]
