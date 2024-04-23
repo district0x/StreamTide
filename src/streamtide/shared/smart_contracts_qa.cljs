0f82e39e3b66ccec7aa1efc356f92c6edcc5db93
ginesdt <ginesdt@gmail.com>
2024-04-23 16:33:09 +0200

    update qa contract info

--- src/streamtide/shared/smart_contracts_qa.cljs
+++ src/streamtide/shared/smart_contracts_qa.cljs
@@ -1,5 +1,5 @@
 (ns streamtide.shared.smart-contracts-qa)
   (def smart-contracts
     {:migrations {:name "Migrations" :address "0x3616f333628b61CEbfddCfbe001A90c4C8feEF02"}
-     :streamtide {:name "MVPCLR" :address "0xda35804a3ad0A27706118D09acD47C775E52b884"}
+     :streamtide {:name "MVPCLR" :address "0x3b038Bb1fdB38CF7CC19c7917D6979A1f5454897"}
      :streamtide-fwd {:name "MutableForwarder" :address "0x26561312D23e9AfF9FbB6387D8911729705C56e3" :forwards-to :streamtide}})
