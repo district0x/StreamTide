97d22620742f0bc9dd6ecc6f74d02d2bd098b80f
ginesdt <ginesdt@gmail.com>
2024-06-25 10:00:37 +0200

    fix notifications async

--- src/streamtide/server/notifiers/notifiers.cljs
+++ src/streamtide/server/notifiers/notifiers.cljs
@@ -44,43 +44,47 @@
 
 (defn notify-announcement [announcement]
   "Sends a notification to all subscribers when there is a new announcement"
-  (let [notification {:category :announcements
-                      :title "New Announcement"
-                      :body (:announcement/text announcement)}
-        notification-entries (stdb/get-notification-categories {:notification/category (name :notification-category/announcements)
-                                                                :notification/enable true})]
-    (notify-all-types notification-entries notification)))
+  (safe-go
+    (let [notification {:category :announcements
+                        :title "New Announcement"
+                        :body (:announcement/text announcement)}
+          notification-entries (<? (stdb/get-notification-categories {:notification/category (name :notification-category/announcements)
+                                                                  :notification/enable true}))]
+      (<? (notify-all-types notification-entries notification)))))
 
 (defn notify-new-content [{:keys [:user/address] :as creator}]
   "Sends a notification to the supported of a creator when there is new content"
-  (let [notification {:title "New content from supported patron"
-                      :body (gstring/format "The creator you support \"%s\" has added new content" (:user/name creator))}
+  (safe-go
+    (let [notification {:title "New content from supported patron"
+                        :body (gstring/format "The creator you support \"%s\" has added new content" (:user/name creator))}
 
-        addresses (map :user/source-user (stdb/get-user-content-permissions {:user/target-user address}))
-        enabled-notifications (stdb/get-notification-categories {:user/addresses addresses
-                                                                 :notification/category (name :notification-category/patron-publications)
-                                                                 :notification/enable true})]
-    (notify-all-types enabled-notifications notification)))
+          addresses (map :user/source-user (stdb/get-user-content-permissions {:user/target-user address}))
+          enabled-notifications (<? (stdb/get-notification-categories {:user/addresses addresses
+                                                                   :notification/category (name :notification-category/patron-publications)
+                                                                   :notification/enable true}))]
+      (<? (notify-all-types enabled-notifications notification)))))
 
 (defn notify-donation [donation]
   "Sends a notification to the receiver of a donation"
-  (let [sender (stdb/get-user (:donation/sender donation))
-        notification {:title "Donation received"
-                      :body (gstring/format "You have received a donation from %s of a value of %s"
-                                            (if (string/blank? (:user/name sender)) (:donation/sender donation) (:user/name sender))
-                                            (shared-utils/format-price (:donation/amount donation) {:coin/decimals 18 :coin/symbol "ETH"}))}
-        notification-entries (stdb/get-notification-categories {:user/address (:donation/receiver donation)
-                                                                :notification/category (name :notification-category/donations)
-                                                                :notification/enable true})]
-  (notify-all-types notification-entries notification)))
+  (safe-go
+    (let [sender (stdb/get-user (:donation/sender donation))
+          notification {:title "Donation received"
+                        :body (gstring/format "You have received a donation from %s of a value of %s"
+                                              (if (string/blank? (:user/name sender)) (:donation/sender donation) (:user/name sender))
+                                              (shared-utils/format-price (:donation/amount donation) {:coin/decimals 18 :coin/symbol "ETH"}))}
+          notification-entries (<? (stdb/get-notification-categories {:user/address (:donation/receiver donation)
+                                                                  :notification/category (name :notification-category/donations)
+                                                                  :notification/enable true}))]
+      (<? (notify-all-types notification-entries notification)))))
 
 (defn notify-grants-statuses [grants-status]
   "Sends a notification to all the users whose grant status have changed"
-  (let [approved? (= (:grant/status grants-status) "approved")
-        notification {:title (if approved? "Grant approved!" "Grant rejected")
-                      :body (if approved? "Congratulations! Your grant has been approved. You can now add content and enjoy the benefits of being a creator"
-                                          "Unfortunately, your grant has been rejected. Contact our support team in Discord if you think there is some sort of mistake")}
-        notification-entries (stdb/get-notification-categories {:user/addresses (:user/addresses grants-status)
-                                                                :notification/category (name :notification-category/grant-status)
-                                                                :notification/enable true})]
-  (notify-all-types notification-entries notification)))
+  (safe-go
+    (let [approved? (= (:grant/status grants-status) "approved")
+          notification {:title (if approved? "Grant approved!" "Grant rejected")
+                        :body (if approved? "Congratulations! Your grant has been approved. You can now add content and enjoy the benefits of being a creator"
+                                            "Unfortunately, your grant has been rejected. Contact our support team in Discord if you think there is some sort of mistake")}
+          notification-entries (<? (stdb/get-notification-categories {:user/addresses (:user/addresses grants-status)
+                                                                  :notification/category (name :notification-category/grant-status)
+                                                                  :notification/enable true}))]
+      (<? (notify-all-types notification-entries notification)))))
